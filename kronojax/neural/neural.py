d15607592b3aa7d5e00a95cfbda851a9e3c37a89
Ruizi-Wu <wu.ruizi119@gmail.com>
2024-09-13 15:41:58 +0800

    0913

--- kronojax/neural/neural.py
+++
@@ -1,9 +0,0 @@
-import jax
-import jax.numpy as jnp
-from jax._src.typing import Array
-
-######################################################################
-# TIME EMBEDDING WITH SINES
-######################################################################
-
-def time_embedding()
